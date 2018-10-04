c84cb046b80c367d776af7f53cb6aa830d3f7008
Aude <audechevalie@gmail.com>
2018-10-04 13:51:49 +0200

    Initial Commit

---
+++ FairePasserDesDonnéesEcran1VersEcran2/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FairePasserDesDonnéesEcran1VersEcran2
+//
+//  Created by CHEVALIER on 04/10/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
