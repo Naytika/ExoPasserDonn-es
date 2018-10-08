b3983e39a33f852bdc6d3cf120eb3c24e0b05d82
Aude <audechevalie@gmail.com>
2018-10-08 10:44:41 +0200

    Ecran de Base

---
+++ FairePasserDesDonnéesEcran1VersEcran2/EcranAController.swift
@@ -0,0 +1,25 @@
+//
+//  EcranAController.swift
+//  FairePasserDesDonnéesEcran1VersEcran2
+//
+//  Created by CHEVALIER on 04/10/2018.
+//
+
+import UIKit
+
+class EcranAController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+    
+
+    
+}
+            
+
---
+++ FairePasserDesDonnéesEcran1VersEcran2/EcranBController.swift
@@ -0,0 +1,26 @@
+//
+//  EcranBController.swift
+//  FairePasserDesDonnéesEcran1VersEcran2
+//
+//  Created by CHEVALIER on 04/10/2018.
+//
+
+import UIKit
+
+class EcranBController: UIViewController {
+
+
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+     
+    }
+
+    
+
+
+
+}
